a174aa2e47c034671a932524f6a0e6ac538a9009
Haleema Sadia <157414286+Haleema9024@users.noreply.github.com>
2024-06-10 16:04:27 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,65 @@
+let fruits = ['apple', 'banana', 'mango', 'peach'];
+for(let i = 0; i < fruits.length; i++){
+    console.log(fruits[i]);
+}
+
+
+
+
+let name = "Haleema";
+for (let i = 0; i < name.length; i++) {
+    console.log(name.split(' ').join(' '));
+}
+
+
+
+
+
+// what is the result of adding 5 and 3, and then multiplying the result by 2?
+let result = (5 + 3) * 2;
+console.log(result);
+
+
+
+
+
+
+// Calculate the area of a rectangle with length 10 unit and width 5 units.
+let length = 10; // length of the rectangle
+let width = 5; // width of the rectangle
+// Calculate the area
+let area = length * width;
+console.log("The area of the rectangle is: " + area + " square units");
+
+
+
+
+
+let num1 = 2;
+let num2 = 6;
+console.log("Sum of num1 and num2 is:" + (num1 + num2));
+
+
+
+
+
+let x = 5;
+let y = x++;
+console.log(y);
+console.log(x);
+
+
+
+
+
+let a = 7;
+let b = ++a;
+let c = a--;
+console.log(a); //7
+console.log(b); //8
+console.log(c); //8
+
+
+
+
+
